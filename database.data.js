12118449d0ad63fe782cc0b88b7e697a726d4cea
Baimu7777 <baimuuuuuuu@gmail.com>
2026-02-02 10:45:35 +0800

    Update database.data.js

--- database.data.js
+++ database.data.js
@@ -30,5 +30,54 @@ window.DB_ITEMS = window.DB_ITEMS || [
     score: -52,
     brief: "别一看到双女主就夸女性友好行吗？ ",
     link: "https://www.douban.com/group/topic/474413327/"
+  },{
+    type: "影视",
+    name: "海妖的呼唤：火之岛生存战",
+    en: "사이렌: 불의 섬",
+    score: 31,
+    brief: "",
+    link: "https://www.douban.com/group/topic/474409541/"
+  },{
+    type: "影视",
+    name: "芭比",
+    en: "Barbie",
+    score: -12,
+    brief: "请粉色停止霸凌女性 ",
+    link: "https://www.douban.com/group/topic/474374246/"
+  },{
+    type: "影视",
+    name: "双雌大盗",
+    en: "Frauds",
+    score: -45,
+    brief: "双女主≠女性友好，短短六集但五螙俱全 ",
+    link: "https://www.douban.com/group/topic/474454677/"
+  },{
+    type: "影视",
+    name: "扑克脸S1~S2",
+    en: "Poker Face",
+    score: -71,
+    brief: "女性主角公路片单元剧，我是冲着这个去看的，结果被散落的性缘、vgbtq时不时恶心了一下，还有海量的点子角色和好点子形象、负面描写有野心的女角色的剧情恶心到。",
+    link: "https://www.douban.com/group/topic/474496111/"
+  },{
+    type: "叙事书籍",
+    name: "那不勒斯四部曲",
+    en: "Neapolitan Novels",
+    score: -33,
+    brief: "我承认在过去五年里，莱农和莉拉在一定程度上对某一个时间段的我影响非常深，但我同样承认，从现在的角度与观念看来，它是一个远远不够的非常落伍的作品。它的致命缺点就是俩女主一辈子都在被不同的男人吃，故事的主线是反抗与独立，但抗争很多，苦难与失败更多，虐女更多。",
+    link: "https://www.douban.com/group/topic/474480833/"
+  },{
+    type: "叙事书籍",
+    name: "丹妮斯的重生",
+    en: "",
+    score: 43,
+    brief: "阅读体验感很好，打分下来的唯一感受是，盧子干的事能不能不算在女身上！扣了很多的分啊",
+    link: "https://www.douban.com/group/topic/474501754/"
+  },{
+    type: "影视",
+    name: "老练律师",
+    en: "Matlock",
+    score: -38,
+    brief: "女主剧为何一定要掺入致死量性缘关系？ \n题材我是很感兴趣的 可惜太多男角色了 太多性缘关系 这剧里的每个女性仿佛离了男的or女的都活不了 几个女角色之前的对手戏很好看 但就像是在吃掺了屎的巧克力 主角madeline 有个老公 一直pua她 有个男孙 一直想找他那xd的爹 重要角色olympia和她老公拉拉扯扯 这期间有一段时间还有另外的男朋友 结尾好像也没离掉吧 ",
+    link: "https://www.douban.com/group/topic/474509780/"
   }
 ];
